0841fb51f4e33473f6cd5b6e4eedbbfeb1d897c4
alicedaurelio <86382764+alicedaurelio@users.noreply.github.com>
2022-04-07 05:35:39 -0300

    Add files via upload

---
+++ ProjetoTarefaBD.sql
@@ -0,0 +1,12 @@
+create database projetotarefa;
+
+use projetotarefa;
+
+create table tbtarefa(
+	titulo varchar(50),
+    assunto varchar(50),
+    dataTarefa date,
+    horaTarefa time,
+	importancia boolean
+);
+
